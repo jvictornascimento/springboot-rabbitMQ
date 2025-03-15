58143edeb29ea41001a8fc1611ba4578c6cb7c2c
jvictornascimento <victojoao789@gmail.com>
2025-03-15 15:23:09 -0300

    Ms-send-email created

---
+++ ms-send-email/src/main/java/com/jvictornascimento/mssendemail/MsSendEmailApplication.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.mssendemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSendEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSendEmailApplication.class, args);
+	}
+
+}
---
+++ ms-send-email/src/test/java/com/jvictornascimento/mssendemail/MsSendEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.mssendemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSendEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
