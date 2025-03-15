27307855629bce17563628849eecbb03afc5c26f
jvictornascimento <victojoao789@gmail.com>
2025-03-15 16:51:11 -0300

    Ms-newsletter created

---
+++ ms-newsletter/src/main/java/com/jvictornascimento/msnewsletter/MsNewsletterApplication.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.msnewsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNewsletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNewsletterApplication.class, args);
+	}
+
+}
---
+++ ms-newsletter/src/test/java/com/jvictornascimento/msnewsletter/MsNewsletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvictornascimento.msnewsletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNewsletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
